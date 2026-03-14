79e9d534634a98ac1d6fe4db6f68507497641912
OssieFromDK <mrcraftonyt@gmail.com>
2026-03-14 12:45:54 -0500

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -1,74 +1,28 @@
 return {
 	["idkman"] = {
-		["LeftHip"]={
-			["CFrame"]=cn(0.182,0,0)*ca(80.735,0,-0),
-			["TimeLength"]=0,
-		},
-		["RightHip"]={
-			["CFrame"]=cn(-0.296,0,0)*ca(81.093,0,-0),
-			["TimeLength"]=0,
-		},
-		["RightShoulder"]={
-			["CFrame"]=ca(31.387,0,-19.899),
-			["TimeLength"]=0,
-		},
-		["LeftShoulder"]={
-			["CFrame"]=ca(27.474,0,12.54),
-			["TimeLength"]=0,
-		},
-		["LeftElbow"]={
-			["CFrame"]=ca(-0,0,1.379),
-			["TimeLength"]=0,
-		},
-	},
-	[2] = {
-		["LeftHip"]={
-			["CFrame"]=cn(0.182,-0.037,-0.228)*ca(80.735,0,-0),
-			["TimeLength"]=0.699,
-		},
-		["RightHip"]={
-			["CFrame"]=cn(-0.296,-0.033,-0.214)*ca(81.093,0,-0),
-			["TimeLength"]=0.699,
-		},
-		["RightShoulder"]={
-			["CFrame"]=cn(-0.034,0.007,-0.164)*ca(41.194,-3.55,-19.71),
-			["TimeLength"]=0.699,
-		},
-		["LeftShoulder"]={
-			["CFrame"]=cn(0.014,-0.014,-0.166)*ca(35.887,1.864,12.403),
-			["TimeLength"]=0.699,
-		},
-		["LeftElbow"]={
-			["CFrame"]=ca(-0,0,1.379),
-			["TimeLength"]=0.699,
-		},
-	},
-	[3] = {
-		["RightHip"]={
-			["CFrame"]=cn(-0.296,-0.033,-0.214)*ca(81.093,0,-0),
-			["TimeLength"]=-0.083,
-		},
-		["LeftHip"]={
-			["CFrame"]=cn(0.182,-0.037,-0.228)*ca(80.735,0,-0),
-			["TimeLength"]=-0.083,
-		},
-		["RightShoulder"]={
-			["CFrame"]=cn(0.002,0.084,-0.093)*ca(41.194,-3.55,-19.71),
-			["TimeLength"]=-0.083,
-		},
-		["LeftShoulder"]={
-			["CFrame"]=cn(-0.009,0.072,-0.102)*ca(35.887,1.864,12.403),
-			["TimeLength"]=-0.083,
-		},
-	},
-	[4] = {
-		["RightShoulder"]={
-			["CFrame"]=cn(-0.079,-0.084,-0.248)*ca(41.194,-3.55,-19.71),
-			["TimeLength"]=0.183,
-		},
-		["LeftShoulder"]={
-			["CFrame"]=cn(0.042,-0.118,-0.243)*ca(35.887,1.864,12.403),
-			["TimeLength"]=0.183,
-		},
-	},
+		{Time = 0, Data = {
+			["LeftHip"] = CFrame.new(0.182, 0, 0) * CFrame.Angles(math.rad(80.735), 0, 0),
+			["RightHip"] = CFrame.new(-0.296, 0, 0) * CFrame.Angles(math.rad(81.093), 0, 0),
+			["RightShoulder"] = CFrame.Angles(math.rad(31.387), 0, math.rad(-19.899)),
+			["LeftShoulder"] = CFrame.Angles(math.rad(27.474), 0, math.rad(12.54)),
+			["LeftElbow"] = CFrame.Angles(0, 0, math.rad(1.379)),
+		}},
+		{Time = 0.699, Data = {
+			["LeftHip"] = CFrame.new(0.182, -0.037, -0.228) * CFrame.Angles(math.rad(80.735), 0, 0),
+			["RightHip"] = CFrame.new(-0.296, -0.033, -0.214) * CFrame.Angles(math.rad(81.093), 0, 0),
+			["RightShoulder"] = CFrame.new(-0.034, 0.007, -0.164) * CFrame.Angles(math.rad(41.194), math.rad(-3.55), math.rad(-19.71)),
+			["LeftShoulder"] = CFrame.new(0.014, -0.014, -0.166) * CFrame.Angles(math.rad(35.887), math.rad(1.864), math.rad(12.403)),
+			["LeftElbow"] = CFrame.Angles(0, 0, math.rad(1.379)),
+		}},
+		{Time = 0.616, Data = {
+			["RightHip"] = CFrame.new(-0.296, -0.033, -0.214) * CFrame.Angles(math.rad(81.093), 0, 0),
+			["LeftHip"] = CFrame.new(0.182, -0.037, -0.228) * CFrame.Angles(math.rad(80.735), 0, 0),
+			["RightShoulder"] = CFrame.new(0.002, 0.084, -0.093) * CFrame.Angles(math.rad(41.194), math.rad(-3.55), math.rad(-19.71)),
+			["LeftShoulder"] = CFrame.new(-0.009, 0.072, -0.102) * CFrame.Angles(math.rad(35.887), math.rad(1.864), math.rad(12.403)),
+		}},
+		{Time = 0.799, Data = {
+			["RightShoulder"] = CFrame.new(-0.079, -0.084, -0.248) * CFrame.Angles(math.rad(41.194), math.rad(-3.55), math.rad(-19.71)),
+			["LeftShoulder"] = CFrame.new(0.042, -0.118, -0.243) * CFrame.Angles(math.rad(35.887), math.rad(1.864), math.rad(12.403)),
+		}},
+	}
 }
